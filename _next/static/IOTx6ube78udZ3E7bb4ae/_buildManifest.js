6489aa5294c758dfb2cc88f1240d80d4212c0b73
kokoichi206 <kokoichi206@users.noreply.github.com>
2022-04-27 15:10:51 +0000

    deploy: bffe943e6418887903c999b0b6633e3c015c4294

--- _next/static/IOTx6ube78udZ3E7bb4ae/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/46c9f256354d0e47.css","static/chunks/pages/index-7922a0f10359ceaa.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
